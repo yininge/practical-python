7d840333d104e4bcdc228c5a15801ac817f02d05
yininge <yining.huang0716@gmail.com>
2020-06-24 15:30:59 -0700

    1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,23 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, 'rt') as file:
+        next(file)
+        rows = csv.reader(file)
+        for row in rows:
+            total += int(row[1]) * float(row[2])
+        return total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+
+cost = portfolio_cost(filename)
+print(f"Total cost {cost:0.2f}")
